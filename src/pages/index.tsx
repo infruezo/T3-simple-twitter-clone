c11d084216cf823530220ae2056091330f703dcc
belghoul <belghoul@systemd.se>
2023-04-20 02:48:06 +0100

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
 
-// import { api } from "~/utils/api";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -17,8 +17,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
